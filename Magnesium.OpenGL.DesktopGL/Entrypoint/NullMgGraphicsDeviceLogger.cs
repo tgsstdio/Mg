d9cd413999654af8fdbc31c1938a1f4e3ac5a20b
David Young <tgs.wifi@gmail.com>
2016-09-20 01:47:09 +1000

    Moving files around

---
+++ Magnesium.OpenGL.DesktopGL/Entrypoint/NullMgGraphicsDeviceLogger.cs
@@ -0,0 +1,13 @@
+namespace Magnesium.OpenGL.DesktopGL
+{
+	public class NullMgGraphicsDeviceLogger : IMgGraphicsDeviceLogger
+	{
+		#region IGraphicsDeviceLogger implementation
+		public void Log (string message)
+		{
+	
+		}
+		#endregion
+	}
+
+}
