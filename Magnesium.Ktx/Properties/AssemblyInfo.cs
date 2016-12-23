642531da0b1c56178cf72a4b743e332e2b6a6007
David Young <tgs.wifi@gmail.com>
2016-12-24 02:51:12 +1100

    Adjusted AssemblyInfo

--- Magnesium.Ktx/Properties/AssemblyInfo.cs
+++ Magnesium.Ktx/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("MonoGame.Textures.Ktx")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyTitle("Magnesium.Ktx")]
+[assembly: AssemblyDescription("Loads Khronos .ktx image file format in Magnesium.")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
