0d549531a34206e9e5b178c7763805f81da57108
David Young <tgs.wifi@gmail.com>
2017-02-11 17:12:58 +1100

    Bugfix - Processing empty/null descriptors in command buffers
    Mg.GL 6.0.0 crashes when processing a command buffer without a bound
    descriptor set

---
+++ tests/Magnesium.OpenGL.UnitTests/MockGLDescriptorPoolResource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Magnesium.OpenGL.UnitTests
+{
+    internal class MockGLDescriptorPoolResource<T> : IGLDescriptorPoolResource<T>
+    {
+        public uint Count
+        {
+            get
+            {
+                return (uint) Items.Length;
+            }
+        }
+
+        public T[] Items
+        {
+            get;
+            set;
+        }
+
+        public bool Allocate(uint request, out GLPoolResourceTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Free(GLPoolResourceTicket ticket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
