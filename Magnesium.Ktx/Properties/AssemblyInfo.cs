d01454d0299f3ac89a49bc4590415d68dc492098
David Young <tgsstdio@gmail.com>
2017-09-02 01:47:17 +1000

    Fixing Mg w.r.t Vk >1.0.50 (#15)
    * Initial fix
    
    * TriangleDemo Alpha build one
    
    * More changes
    
    * TriangleDemo alpha completed
    
    * TextureDemo alpha updated nuget
    
    * Fixing alpha nuget stuff
    
    * First beta upload
    
    * Beta release to nuget
    
    * Removed enabled features
    
    * FIX: Auto-selects depth-format instead of using user-supplied value
    
    * Updating build and documentation
    
    * Update most projects to Mg v5.2.8-beta packages

--- Magnesium.Ktx/Properties/AssemblyInfo.cs
+++ Magnesium.Ktx/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("David")]
+[assembly: AssemblyCopyright ("Copyright David Young (c) 2016, 2017")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.4")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
--- Magnesium/Properties/AssemblyInfo.cs
+++ Magnesium/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("David Young 2016")]
+[assembly: AssemblyCopyright("Copyright (c) 2016, 2017 David Young")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -16,7 +16,7 @@
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("5.0.4.*")]
+[assembly: AssemblyVersion("5.2.8")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
