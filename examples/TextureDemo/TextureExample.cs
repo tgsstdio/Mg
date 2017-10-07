adb4b161088172aa82ff9235633d853dd3ec20ac
David Young <tgs.wifi@gmail.com>
2017-10-07 14:51:24 +1100

    Migrated TextureDemo to .NET Standard
    Bug: no texture displayed

--- examples/TextureDemo/TextureExample.cs
+++ examples/TextureDemo/TextureExample.cs
@@ -35,8 +35,10 @@ namespace TextureDemo
             var createInfo = new MgGraphicsDeviceCreateInfo
             {
                 Samples = MgSampleCountFlagBits.COUNT_1_BIT,
-                Color = MgFormat.R8G8B8A8_UINT,
-                DepthStencil = MgFormat.X8_D24_UNORM_PACK32,
+                Color = MgColorFormatOption.AUTO_DETECT,
+              //  OverrideColor = MgFormat.R8G8B8A8_UINT,
+                DepthStencil = MgDepthFormatOption.AUTO_DETECT,
+                //OverrideDepthStencil = MgFormat.X8_D24_UNORM_PACK32,
                 Width = 1280,
                 Height = 720,
             };
