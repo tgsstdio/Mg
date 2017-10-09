39dd97e32d8329bb7f209d4bec02e11b01bd9319
David Young <tgs.wifi@gmail.com>
2017-10-10 00:55:49 +1100

    Fixed issue with LineStrip
    Added unit tests
    
    Fixing unit tests

---
+++ tests/GL002/GL002.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Magnesium.OpenGL.DesktopGL.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Magnesium.OpenGL.DesktopGL.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0ded9af-ab03-425a-bfab-83c239bed98a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
