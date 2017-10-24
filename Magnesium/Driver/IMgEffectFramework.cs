b282ece66e9ba12a5a68b5c230dfef95e9094cf3
David Young <tgs.wifi@gmail.com>
2017-10-24 17:23:37 +1100

    Moving files around

---
+++ Magnesium/Driver/IMgEffectFramework.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Magnesium
+{
+    // SUPER CLASS of graphics device (window-out) and offscreen (fbo)
+    // for coding a pipeline around 
+    // TODO : rename this class     
+    public interface IMgEffectFramework : IDisposable
+    {
+        MgRect2D Scissor
+        {
+            get;
+        }
+
+        MgViewport Viewport
+        {
+            get;
+        }
+
+        MgRenderPassCreateInfo RenderpassInfo { get; }
+
+        IMgRenderPass Renderpass
+        {
+            get;
+        }
+
+        IMgFramebuffer[] Framebuffers
+        {
+            get;
+        }
+    }
+}
+
--- Magnesium/Driver/IMgGraphicsDevice.cs
+++ Magnesium/Driver/IMgGraphicsDevice.cs
@@ -1,38 +1,9 @@
-﻿using System;
-
-namespace Magnesium
+﻿namespace Magnesium
 {
-    // SUPER CLASS of graphics device (window-out) and offscreen (fbo)
-        // for coding a pipeline around 
-    // TODO : rename this class     
-    public interface IMgEffectFramework : IDisposable
-    {
-        MgRect2D Scissor
-        {
-            get;
-        }
-
-        MgViewport Viewport
-        {
-            get;
-        }
-
-        MgRenderPassCreateInfo RenderpassInfo { get; }
-
-        IMgRenderPass Renderpass
-        {
-            get;
-        }
-
-        IMgFramebuffer[] Framebuffers
-        {
-            get;
-        }
-    }
 
     // always (window-out)
-	// TODO : rename this class 
-	public interface IMgGraphicsDevice : IMgEffectFramework
+    // TODO : rename this class 
+    public interface IMgGraphicsDevice : IMgEffectFramework
     {
 		bool DeviceCreated();
 		bool IsDisposed();
